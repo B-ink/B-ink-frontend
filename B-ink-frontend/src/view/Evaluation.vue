a83673cd2b2699f5cbf42b81b48c6218d9593252
161250053 <161250053@smail.nju.edu.cn>
2018-08-31 14:57:41 +0800

    fix some bugs

--- B-ink-frontend/src/view/Evaluation.vue
+++ B-ink-frontend/src/view/Evaluation.vue
@@ -1,7 +1,6 @@
 <template>
-<el-container :direction="vertical" style="height: 100%">
-  <el-header>Header</el-header>
-  <el-container>
+  <el-container :direction="vertical" style="height: 100%">
+    <el-container>
       <div id="risk-form" style="width: 100%">
         <swiper :options="swiperOption" style="height: 100%; width: 100%">
           <swiper-slide style="height: 100%">
@@ -13,10 +12,8 @@
             <img id="nextIcon" src="../../static/icon/下移.png" width="40px" style="position: absolute; bottom: 0px">
           </swiper-slide>
           <swiper-slide style="height: 100%">
-            <el-form>
-              <el-form-item style="margin-bottom: 10px">
-                <img src="../../static/icon/漏水.png" width="20px">浸水评估
-              </el-form-item>
+            <el-form label-width="80px" label-position="left">
+              <div class="title"><img src="../../static/icon/漏水.png" width="20px">浸水评估</div>
               <el-form-item label="墙面">
                 <el-select v-model="value1" placeholder="请选择">
                   <el-option
@@ -57,9 +54,7 @@
                   </el-option>
                 </el-select>
               </el-form-item>
-              <el-form-item style="margin-bottom: 10px; margin-top: 20px">
-                <img src="../../static/icon/地震.png" width="20px">地震评估
-              </el-form-item>
+              <div class="title"><img src="../../static/icon/地震.png" width="20px">地震评估</div>
               <el-form-item label="建筑材料">
                 <el-select v-model="value5" placeholder="请选择">
                   <el-option
@@ -70,16 +65,15 @@
                   </el-option>
                 </el-select>
               </el-form-item>
-              <el-form-item style="margin-bottom: 10px; margin-top: 20px">
-                <img src="../../static/icon/火灾.png" width="20px">火灾评估
-              </el-form-item>
+              <div class="title"><img src="../../static/icon/火灾.png" width="20px">火灾评估</div>
               <el-form-item label="消防设施">
                 <el-radio-group v-model="radio1">
                   <el-radio
                     v-for="item in radioOptions"
                     :key="item.value"
                     :label="item.value"
-                    :value="item.value">
+                    :value="item.value"
+                    style="display: block; margin: 0;">
                   </el-radio>
                 </el-radio-group>
               </el-form-item>
@@ -87,64 +81,70 @@
           </swiper-slide>
         </swiper>
       </div>
+    </el-container>
   </el-container>
-</el-container>
 </template>
 
 <script type="text/ecmascript-6">
-import ElAside from 'element-ui/packages/aside/src/main'
-import ElRow from 'element-ui/packages/row/src/row'
-import ElMain from 'element-ui/packages/main/src/main'
-import CitySelector from '../components/CitySelector.vue'
-import ElHeader from 'element-ui/packages/header/src/main'
-import 'swiper/dist/css/swiper.css'
-import {swiper, swiperSlide} from 'vue-awesome-swiper'
+  import ElAside from 'element-ui/packages/aside/src/main'
+  import ElRow from 'element-ui/packages/row/src/row'
+  import ElMain from 'element-ui/packages/main/src/main'
+  import CitySelector from '../components/CitySelector.vue'
+  import ElHeader from 'element-ui/packages/header/src/main'
+  import 'swiper/dist/css/swiper.css'
+  import {swiper, swiperSlide} from 'vue-awesome-swiper'
 
-export default {
-  components: {
-    ElHeader,
-    ElMain,
-    ElRow,
-    ElAside,
-    CitySelector,
-    swiper,
-    swiperSlide},
-  name: 'Evaluation',
-  data () {
-    return {
-      swiperOption: {
-        notNextTick: true,
-        autoplay: 3000,
-        direction: 'vertical',
-        grabCursor: true,
-        setWrapperSize: true,
-        autoHeight: true,
-        mousewheelControl: true,
-        observeParents: true
-      },
-      wallOptions: [{value: '乳胶漆墙面'}, {value: '壁纸墙面'}, {value: '硅藻泥墙面'}, {value: '其他'}],
-      floorOptions: [{value: '木地板'}, {value: '瓷砖'}, {value: '其他'}],
-      panelOptions: [{value: '实木板'}, {value: '人造板'}],
-      paintOptions: [{value: '水性漆'}, {value: '油性漆'}],
-      materials: [{value: '砖石'}, {value: '钢筋混凝土'}],
-      radioOptions: [{value: '家中备有灭火器等设备'}, {value: '楼道中备有灭火器等设备'}, {value: '未备有灭火器等设备'}],
-      value1: '',
-      value2: '',
-      value3: '',
-      value4: '',
-      value5: '',
-      radio1: ''
+  export default {
+    components: {
+      ElHeader,
+      ElMain,
+      ElRow,
+      ElAside,
+      CitySelector,
+      swiper,
+      swiperSlide
+    },
+    name: 'Evaluation',
+    data () {
+      return {
+        swiperOption: {
+          notNextTick: true,
+          autoplay: 3000,
+          direction: 'vertical',
+          grabCursor: true,
+          setWrapperSize: true,
+          autoHeight: true,
+          mousewheelControl: true,
+          observeParents: true
+        },
+        wallOptions: [{value: '乳胶漆墙面'}, {value: '壁纸墙面'}, {value: '硅藻泥墙面'}, {value: '其他'}],
+        floorOptions: [{value: '木地板'}, {value: '瓷砖'}, {value: '其他'}],
+        panelOptions: [{value: '实木板'}, {value: '人造板'}],
+        paintOptions: [{value: '水性漆'}, {value: '油性漆'}],
+        materials: [{value: '砖石'}, {value: '钢筋混凝土'}],
+        radioOptions: [{value: '家中备有灭火器等设备'}, {value: '楼道中备有灭火器等设备'}, {value: '未备有灭火器等设备'}],
+        value1: '',
+        value2: '',
+        value3: '',
+        value4: '',
+        value5: '',
+        radio1: ''
+      }
     }
   }
-}
 </script>
 
 <style lang="stylus">
-#nextIcon
-  position fixed
-  left 45%
-#risk-form
-  margin 10px
-.el-form-item
-  margin-bottom 10px
+  #nextIcon
+    position fixed
+    left 45%
+
+  #risk-form
+    margin 10px
+
+  .el-form-item
+    margin-bottom 10px
+
+  .title
+    margin 30px 0 15px 0
 </style>
