9ed04ce5b226ff260a3d28ac4c721a5a1b340394
161250053 <161250053@smail.nju.edu.cn>
2018-08-31 13:37:49 +0800

    dev: add Login Module

--- B-ink-frontend/src/router/index.js
+++ B-ink-frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Evaluation from '../view/Evaluation.vue'
+import Login from '../view/Login.vue'
 
 Vue.use(Router)
 
@@ -8,8 +9,17 @@ export default new Router({
   routes: [
     {
       path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/evaluation',
       name: 'Evaluation',
       component: Evaluation
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
---
+++ B-ink-frontend/src/view/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class='login'>
+    <div class="login_content">
+      <el-input class="input" placeholder="用户名"></el-input>
+      <el-input class="input" type="password" placeholder="密码"></el-input>
+      <el-button class="btn login_btn" type="primary">登录</el-button>
+      <el-button class="btn sign_btn">注册</el-button>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: 'Login'
+  }
+</script>
+
+<style lang="stylus">
+  .login
+    margin-top 50%
+    .btn, .input
+      display block
+      margin 20px auto
+      width: 60%
+</style>
