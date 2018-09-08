eb70b2a501e3c31f6e72523a74cc94232f3bbb54
161250053 <161250053@smail.nju.edu.cn>
2018-09-08 18:20:32 +0800

    dev

--- B-ink-frontend/src/view/MyInsurance.vue
+++ B-ink-frontend/src/view/MyInsurance.vue
@@ -1,20 +1,22 @@
 <template>
   <div class='my-insurance' style="position: relative">
     <div @click="goEvaluation"
-         style="position: absolute; top: 30px; left: 20px; color: black; font-weight: 500; text-shadow: 0 14px 24px rgba(50,48,58,.25);">
+         style="position: absolute; top: 35px; left: 20px; color: black; font-weight: 500; text-shadow: 0 14px 24px rgba(50,48,58,.25);">
       <i class="el-icon-back"></i>
       去投保
     </div>
-    <div style="font-size: 30px; margin-top: 60px; margin-bottom: 15px;">
+    <div style="position: absolute;right: 20px; top: 30px; font-size: 24px;">
       我的保单
     </div>
-    <div v-for="(insurance, key) in insurances" :key="key"
-         style="position: relative;background-color: #fff;padding: 20px; margin: 20px 0; border-radius: 10px;">
+    <div style="margin-top: 60px">
+      <div v-for="(insurance, key) in insurances" :key="key"
+           style="position: relative;background-color: #fff;padding: 20px; margin: 20px 0; border-radius: 10px;">
         <div style="border-bottom: 1px dashed rgba(7,17,27,0.1); padding-bottom: 10px;">
           <div class="line"><span class="label">投保时间  </span> {{insurance.time}}</div>
         </div>
         <div style="padding: 10px 0 20px 0; border-bottom: 1px dashed rgba(7,17,27,0.1)">
-          <div v-if="insurance.city !== ''" class="line"><span class="label">城市  </span>{{getCity(insurance.city)}}</div>
+          <div v-if="insurance.city !== ''" class="line"><span class="label">城市  </span>{{getCity(insurance.city)}}
+          </div>
           <div v-if="insurance.wall !== ''" class="line"><span class="label">墙面</span> {{insurance.wall}}</div>
           <div v-if="insurance.floor !== ''" class="line"><span class="label">地板</span> {{insurance.floor}}</div>
           <div v-if="insurance.bar !== ''" class="line"><span class="label">板材</span> {{insurance.bar}}</div>
@@ -23,12 +25,14 @@
           <div v-if="insurance.radio1 !== ''" class="line"><span class="label">防火设施</span> {{insurance.radio1}}</div>
         </div>
         <div style="text-align: right; padding-top: 20px;">
-          <div class="line"><span  style="display: inline-block; margin-right: 10px; color: #999">保险额度: ￥{{Number(insurance.money)}}    </span><span class="line">保险费: <span style="color: red">￥{{Number(insurance.expense)}}</span></span></div>
+          <div class="line"><span style="display: inline-block; margin-right: 10px; color: #999">保险额度: ￥{{Number(insurance.money)}}    </span><span
+            class="line">保险费: <span style="color: red">￥{{Number(insurance.expense)}}</span></span></div>
         </div>
-      <i class="coupon-circle coupon-circle-left"></i>
-      <i class="coupon-circle coupon-circle-right"></i>
+        <i class="coupon-circle coupon-circle-left"></i>
+        <i class="coupon-circle coupon-circle-right"></i>
+      </div>
+      <div v-if="insurances.length === 0">您还没有保单</div>
     </div>
-    <div v-if="insurances.length === 0">您还没有保单</div>
   </div>
 </template>
 
